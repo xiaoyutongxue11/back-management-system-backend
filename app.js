6054dbd25b469862292f310ec3ef4f5354006637
tyy123 <1572235056@qq.com>
2024-08-21 22:49:18 +0800

    后端环境搭建

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+// 当extended为false时，req.body只能解析application/x-www-form-urlencoded（数组/字符串）格式的数据，为true时，可以解析application/json（任意）格式的数据
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
