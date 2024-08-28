5a04a4cb6bea885ff92bb4c0ad634e86925086b8
tyy123 <1572235056@qq.com>
2024-08-28 22:23:44 +0800

    使用joi对登录注册参数进行校验

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
+// 导入mysql数据库
+// npm install mysql
 const mysql = require("mysql");
 
+// 创建与数据库的连接
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
@@ -7,4 +10,5 @@ const db = mysql.createPool({
   database: "back_system",
 });
 
-module.export = db;
+// 对外暴露数据库
+module.exports = db;
